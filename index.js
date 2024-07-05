1de00e18b009d1f5a57fccb09d6774523dd4a3ec
ricardogulias <ricardo.gulias@outlook.com>
2024-07-05 10:18:25 -0300

    Meu primeiro programa de desafio da DIO.

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
-let nome = "felipe";
-console.log(nome);
+let nomeHeroi = "Ricardo";
+let quantidadeXP = 12000;
+let qualificacaoHeroi;
+
+if (quantidadeXP <= 1000) {
+    qualificacaoHeroi = "Ferro";
+} else if (quantidadeXP >= 1001 && quantidadeXP <= 2000) {
+    qualificacaoHeroi = "Bronze";
+} else if (quantidadeXP >= 2001 && quantidadeXP <= 5000) {
+    qualificacaoHeroi = "Prata Ouro";
+} else if (quantidadeXP >= 5000 && quantidadeXP <= 8000) {
+    qualificacaoHeroi = "Platina Diamante";
+} else if (quantidadeXP >= 8001 && quantidadeXP <= 9000) {
+    qualificacaoHeroi = "Ascendente";
+} else if (quantidadeXP >= 9001 && quantidadeXP <= 10000) {
+        qualificacaoHeroi = "Imortal";
+} else if (quantidadeXP >= 10001) {
+    qualificacaoHeroi = "Radiante";
+}
+console.log("O Heroi " + nomeHeroi + " tem " + quantidadeXP + " XPs e sua classificação é " + qualificacaoHeroi + ".");
+
+
